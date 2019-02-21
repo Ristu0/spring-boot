2cdbd7b187219d51a44ea06009c78a7b7efe5a85
xuwk <47849582+Ristu0@users.noreply.github.com>
2019-02-21 17:35:57 +0800

    Update SpringbootApplication.java

--- src/main/java/com/baizhi/SpringbootApplication.java
+++ src/main/java/com/baizhi/SpringbootApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.baizhi.dao")
 public class SpringbootApplication {
     public static void main(String[] args) {
+
+        
         SpringApplication.run(SpringbootApplication.class,args);
     }
 }
